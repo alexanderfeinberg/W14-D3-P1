4ad2d56846c2b265cbf7a53c918a34551f6fb4bf
Alexander Feinberg <feinberg.alexander@gmail.com>
2022-10-05 15:19:14 +0000

    Update package-lock.json and index.js

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const UseState = () => {
   const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
   return (
     <div className={theme}>
       <h1>UseState Component</h1>
@@ -19,6 +20,21 @@ const UseState = () => {
         }}
       >
         Light
+      </button>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          return setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          return setCount(count - 1);
+        }}
+      >
+        Decrement
       </button>
     </div>
   );
